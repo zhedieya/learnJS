1700e0fc651ca145d854fbb08c69fdc1da69ad03
zhedieya <318992691@qq.com>
2022-07-07 23:34:45 +0800

    test

--- 一些手写/对象转数组.js
+++ 一些手写/对象转数组.js
@@ -20,3 +20,14 @@ let obj={
 
 // 测试merge
 
+// 手写call   思路就是通过对象调用函数隐式绑定来将this的指向变更为obj
+Function.prototype._call = function (obj, ...arguments) {
+  // 因为调用call时传入指向undefined或者null，this会指向window，所以这儿需处理一下
+  obj = obj || window;
+  // Symbol是唯一的，防止重名key
+  const func = Symbol();
+  // 将func设置为obj的属性，值为this，这样obj.func时this便指向obj
+  obj[func] = this;
+  // 执行，返回执行值
+  return obj[func](...arguments);
+}
